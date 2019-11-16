e55077e0fdbc7fbb6e32144814c16b0ff14d4695
Mutugi Mutuma <mutugimutuma@gmaul.com>
2019-11-16 20:40:52 +0300

    Seperate server from app

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,50 @@
-const { Pool, Client } = require('pg');
+/* eslint-disable */
+const http = require('http');
+const app = require('./app');
 
-const connectionString = 'postgressql://devc:devc123@localhost:5432/devc';
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-const pool = new Pool({
-  connectionString: connectionString
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+  switch (error.code) {
+    case 'EACCESS':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Sends back response object to whoevers making the http call : (req,res) => {}
+const server = http.createServer(app);
+// install nodemon to enable a form of live reload to serve nodemon server
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+  console.log('Listening on ' + port);
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
-const client = new Client({
-  connectionString: connectionString
-});
-
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  client.end();
-});
+server.listen(port);
