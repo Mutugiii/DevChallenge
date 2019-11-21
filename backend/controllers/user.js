dc0e7127e5e01ca7bbfd8bf6f9416d02670e98d6
Mutugi Mutuma <mutugimutuma@gmaul.com>
2019-11-21 18:21:02 +0300

    Add role based auth routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,46 +1,45 @@
+/* eslint-disable no-shadow */
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {
-  pool,
-} = require('../db/user');
+const { pool } = require('../db/user');
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const data = {
-      name: req.body.name,
-      email: req.body.email,
-      password: hash,
-      role: req.body.role,
-    };
-    pool.connect((err, client) => {
-      const query = 'INSERT INTO users(name, email, password, role) VALUES($1, $2, $3, $4) RETURNING *';
-      const values = [data.name, data.email, data.password, data.role];
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+      };
+      pool.connect((err, client) => {
+        const query = 'INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *';
+        const values = [data.name, data.email, data.password];
 
-      client.query(query, values)
-        .then((result) => {
-          res.status(201).json({
-            message: 'User successfuly created',
-            result: result.rows[0],
-          })
+        client.query(query, values).then((result) => {
+          res
+            .status(201)
+            .json({
+              message: 'User successfuly created',
+              result: result.rows[0],
+            })
             .catch((error) => {
               res.status(500).json({
                 error,
               });
             });
         });
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+      });
     });
-  }).catch((error) => {
-    res.status(500).json({
-      error,
-    });
-  });
 };
 
 exports.login = (req, res, next) => {
-  const {
-    email,
-    password,
-  } = req.body;
+  const { email, password } = req.body;
 
   pool.connect((err, client, done) => {
     client.query(
@@ -53,39 +52,48 @@ exports.login = (req, res, next) => {
             message: 'User not found',
           });
         } else {
-          const hash = result.rows[0].password;
-          const userId = result.rows[0].id;
-          const userEmail = result.rows[0].email;
-          bcrypt.compare(password, hash).then((valid) => {
-            if (!valid) {
-              res.status(401).json({
-                error: new Error('Invalid password Entered'),
+          const userRole = result.rows[0].role;
+          if (userRole !== 'Admin') {
+            res.status(400).json({
+              message: 'Forbidden Request',
+            });
+          } else {
+            const hash = result.rows[0].password;
+            const userId = result.rows[0].id;
+            const userEmail = result.rows[0].email;
+            bcrypt
+              .compare(password, hash)
+              .then((valid) => {
+                if (!valid) {
+                  res.status(401).json({
+                    error: new Error('Invalid password Entered'),
+                  });
+                }
+                const data = {
+                  userEmail,
+                  userId,
+                };
+                const token = jwt.sign(
+                  {
+                    data,
+                  },
+                  'RANDOM_TOKEN_SECRET',
+                  {
+                    expiresIn: '24h',
+                  },
+                );
+                res.status(200).json({
+                  userId: data,
+                  token,
+                  message: 'Successful Authentication',
+                });
+              })
+              .catch((error) => {
+                res.status(500).json({
+                  error,
+                });
               });
-            }
-            const data = {
-              userEmail,
-              userId,
-            };
-            const token = jwt.sign({
-              data,
-            },
-            'RANDOM_TOKEN_SECRET', {
-              expiresIn: '24h',
-            });
-            res.status(200).json({
-              userId: data,
-              token,
-              message: 'Successful Authentication',
-            });
-          }).catch((error) => {
-            res.status(500).json({
-              error,
-            });
-          });
-          res.status(201).json({
-            message: 'Successful login',
-            result: result.rows[0],
-          });
+          }
         }
       },
     );
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
+const adminCtrl = require('../controllers/admin');
 
-router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.post('/signup', adminCtrl.signup);
+router.post('/adminlogin', adminCtrl.adminLogin);
 
 module.exports = router;
