7965cbd0b4ce4647ae50c87d499783d3ea7244a3
Mutugi Mutuma <mutugimutuma@gmaul.com>
2019-11-21 18:22:49 +0300

    Remove signup endpoint for employee users

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,45 +1,15 @@
 /* eslint-disable no-shadow */
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { pool } = require('../db/user');
-
-exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const data = {
-        name: req.body.name,
-        email: req.body.email,
-        password: hash,
-      };
-      pool.connect((err, client) => {
-        const query = 'INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *';
-        const values = [data.name, data.email, data.password];
-
-        client.query(query, values).then((result) => {
-          res
-            .status(201)
-            .json({
-              message: 'User successfuly created',
-              result: result.rows[0],
-            })
-            .catch((error) => {
-              res.status(500).json({
-                error,
-              });
-            });
-        });
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error,
-      });
-    });
-};
+const {
+  pool,
+} = require('../db/user');
 
 exports.login = (req, res, next) => {
-  const { email, password } = req.body;
+  const {
+    email,
+    password,
+  } = req.body;
 
   pool.connect((err, client, done) => {
     client.query(
@@ -73,15 +43,12 @@ exports.login = (req, res, next) => {
                   userEmail,
                   userId,
                 };
-                const token = jwt.sign(
-                  {
-                    data,
-                  },
-                  'RANDOM_TOKEN_SECRET',
-                  {
-                    expiresIn: '24h',
-                  },
-                );
+                const token = jwt.sign({
+                  data,
+                },
+                'RANDOM_TOKEN_SECRET', {
+                  expiresIn: '24h',
+                });
                 res.status(200).json({
                   userId: data,
                   token,
