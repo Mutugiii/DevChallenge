1ff60ea0551b3d2861cab7953716f12670019daa
Mutugi Mutuma <mutugimutuma@gmaul.com>
2019-12-07 15:30:15 +0300

    Add routes to user and articles

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const userRoutes = require('./routers/user');
+const contentRoutes = require('./routers/operation');
+
+const connectionString = 'postgressql://devc:devc123@localhost:5432/devc';
+
+const pool = new Pool({
+  connectionString,
+});
+pool.on('connect', () => {
+  console.log('Connected to the db');
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +23,7 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.use('/api/v1', userRoutes);
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/content', contentRoutes);
 
 module.exports = app;
